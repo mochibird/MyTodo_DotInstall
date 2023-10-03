35909cf34e9af64e5b338ba450b1d09ce552e0e1
mochibird <yutaro.mochi@gmail.com>
2023-10-03 13:07:32 +0900

    save program

---
+++ work/public/index.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once(__DIR__ . '/../app/config.php');
+
+use MyApp\Database;
+use MyApp\Todo;
+use MyApp\Utils;
+
+$pdo = Database::getInstance();
+
+$todo = new Todo($pdo);
+$todo->processPost();
+$todos = $todo->getAll();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>My Todos</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+        <div class="container">
+            <h1>Todos</h1>
+            <span class="purge">Purge</span>
+        </div>
+        <form action="?action=add" method="post">
+            <input type="text" name="title" placeholder="Type new todo.">
+        </form>
+
+        <ul>
+            <?php foreach ($todos as $todo) : ?>
+                <li data-id="<?= Utils::h($todo->id); ?>">
+                    <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+                    <span>
+                        <?= Utils::h($todo->title); ?>
+                    </span>
+                    <span class="delete">x</span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </main>
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
